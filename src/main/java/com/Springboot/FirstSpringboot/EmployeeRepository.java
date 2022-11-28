e9918f63f8dd8da86b90751f2b314e3722d0c2e6
StaragileDevops <116961807+StaragileDevops@users.noreply.github.com>
2022-11-29 02:51:15 +0530

    Delete src directory

--- src/main/java/com/Springboot/FirstSpringboot/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Springboot.FirstSpringboot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
-
-}
